fd596f96d0e33340ef78ff2e208ba89f4328359a
CluelessCoder73 <beanhaze012@gmail.com>
2024-12-30 00:56:01 +0000

    Update vdscript_to_cpf_v1.1.0.py
    just the help section

--- vdscript_to_cpf_v1.1.0.py
+++ vdscript_to_cpf_v1.1.0.py
@@ -5,7 +5,9 @@ import os
 # Script: vdscript_to_cpf_v1.1.0.py
 # Description:
 # This script converts VirtualDub or VirtualDub2 .vdscript files into 
-# Cuttermaran .cpf project files.
+# Cuttermaran .cpf project files. It is designed to help users quickly 
+# create Cuttermaran project files from existing VirtualDub cuts.
+# 
 # This script was tested and works with:
 # - Python 3.12.5     
 # - VirtualDub2 (build 44282) .vdscript files
@@ -18,6 +20,15 @@ import os
 # Usage:
 # 1. Specify the paths for the .vdscript file, .cpf file, video file, and audio file.
 # 2. Run the script.
+#
+# Note: Works best with .vdscript files processed by "vdscript_range_adjuster", 
+# which is another one of my python scripts. It can be found on GitHub. 
+# If your cut-in/cut-out points have already been aligned with keyframes, 
+# "vdscript_range_adjuster" is not necessary.
+#
+# Version History:
+# v1.0.0 - Initial script with basic conversion functionality.
+# v1.1.0 - Added segment identifying feature for all output .cpf files.
 # ----------------------------------------------------------------------
 
 def parse_vdscript(filepath):
